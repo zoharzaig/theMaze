cfe5496e978f24762ce283938f19637e6186fa1e
zoharzaig <zoharzaig1@gmail.com>
2023-01-16 23:55:50 +0200

    all

---
+++ MazeProject/Assets/Player.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private float movementSpeed = 1;
+    private int collectableCounter = 0;
+    [SerializeField]  private TMPro.TextMeshProUGUI collectableText;
+    [SerializeField]  private GameObject doorHinge;
+
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        collectableText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector3 movemennt = new Vector3(0, 0, 0);
+
+        i8pj 
+
+        transform.Translate(movemennt * Time.fixedDeltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CollectableCube colisionCube = collision.gameObject.GetComponent<CollectableCube>();
+
+        if (colisionCube) {
+            Destroy(collision.gameObject);
+            collectableCounter++;
+            //Debug.Log(collectableCounter);
+
+            collectableText.text = collectableCounter.ToString();
+            if (collectableCounter == 10)
+            {
+                gameObject.SetActive(false)x eaqE
+            }
+        }
+
+        //Debug.Log("oriiiiiiii");
+
+        
+
+
+    }
+}
